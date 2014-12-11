8c4e2f5a1b8d09bbde47513fd97a5e32cf995992
tanjoin <armatura-quinquiplex@hotmail.co.jp>
2014-12-11 16:42:15 +0900

    いろいろおかしい

--- yjfx/index.js
+++ yjfx/index.js
@@ -1,14 +1,18 @@
 var _positions = [];
 
-function syokokin(resource, need, review) {
-  return 100 * resource / need;
+// 有効証拠金額 ÷ 必要証拠金 × 100 = 証拠金維持率
+function syokokinritsu(resource, need) {
+  if (need != 0) {
+    return resource / need * 100;
+  } else {
+    return " - ";
+  }
 }
 
-function syokokinritsu(need, torihiki) {
-  return 100 * need / torihiki;
+// 算出式：（評価レート）× 10,000通貨 × 4% @ 25倍
+function need(hyoka_rate, tsuka) {
+  return hyoka_rate * tsuka * 4 / 100
 }
-
-function 
 
 function add() {
   var money = document.add_position.money.value;
@@ -26,6 +30,7 @@ function add() {
 }
 
 function show_list() {
+  var hyoka_rate = document.add_position.hyoka_rate.value;
   var resource = document.add_position.resource.value;
   var pip = 0.01;
   var pips = document.add_position.pips.value;
@@ -76,14 +81,15 @@ function show_list() {
       var td3 = tr.insertCell();
       // 損得
       var order = _positions[j];
+      var default_need = need(hyoka_rate, parseFloat(order.maisu));
       var v = (parseFloat(kingaku) * 100 - parseFloat(order.money) * 100) * parseFloat(order.maisu) / 100;
       if (v < 0) {
         td3.style.color = "#F00";
       }
       td3.innerHTML += v;
-      sum += v;
+      sum += default_need + v;
     }
-    td2.innerHTML += sum;
+    td2.innerHTML += syokokinritsu(resource, sum) + "%";
   }
 
   // ボディ(真ん中)
@@ -94,17 +100,20 @@ function show_list() {
   td1.innerHTML += kingaku;
   // 証拠金率
   var td2 = tr.insertCell();
+  var sum = 0.0;
   for (j = 0; j < _positions.length; j++) {
     var td3 = tr.insertCell();
     // 損得
     var order = _positions[j];
+    var default_need = need(hyoka_rate, parseFloat(order.maisu));
     var v = (parseFloat(kingaku) * 100 - parseFloat(order.money) * 100) * parseFloat(order.maisu) / 100;
     if (v < 0) {
       td3.style.color = "#F00";
     }
     td3.innerHTML += v;
+    sum += default_need + v;
   }
-  td2.innerHTML += 0;
+  td2.innerHTML += syokokinritsu(resource, sum) + "%";
 
   // ボディ(+)
   for (i = 0; i < half_pips; i++) {
@@ -115,17 +124,20 @@ function show_list() {
     td1.innerHTML += kingaku;
     // 証拠金率
     var td2 = tr.insertCell();
+    var sum = 0.0;
     for (j = 0; j < _positions.length; j++) {
       var td3 = tr.insertCell();
       // 損得
       var order = _positions[j];
+      var default_need = need(hyoka_rate, parseFloat(order.maisu));
       var v = (parseFloat(kingaku) * 100 - parseFloat(order.money) * 100) * parseFloat(order.maisu) / 100;
       if (v < 0) {
         td3.style.color = "#F00";
       }
       td3.innerHTML += v;
+      sum += default_need + v;
     }
-    td2.innerHTML += 0;
+    td2.innerHTML += syokokinritsu(resource, sum) + "%";
   }
 
   list.appendChild(table);
