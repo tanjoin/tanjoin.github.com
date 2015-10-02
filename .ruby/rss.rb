135cb3c96ed81c20172ea850050e24aa8f069087
tanjoin <armatura-quinquiplex@hotmail.co.jp>
2015-10-02 17:15:11 +0900

    RSS

---
+++ .ruby/rss.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+
+require 'rss'
+
+raw = `git diff --raw --cached`
+
+puts raw
+
+if raw.length > 0 then
+
+  htmls = []
+
+  raw.each_line { |line|
+    data = line.split(' ')
+    file = data[5]
+
+    if file.match(/.*.html/) then
+      htmls.push(file)
+    end
+  }
+
+  if htmls.length > 0 then
+    rss = RSS::Maker.make("1.0") { |maker|
+      maker.channel.about = 'https://tanjo.in/rss.rdf'
+      maker.channel.title = 'tanjoin'
+      maker.channel.description = 'tanjoin の RSS です'
+      maker.channel.link = 'http://tanjo.in'
+      maker.channel.language = 'ja'
+
+      maker.items.do_sort = true
+      maker.items.max_size = 20
+
+      htmls.each { |file|
+        maker.items.new_item { |item|
+          item.link = 'http://tanjo.in/' + file
+          item.title = file
+          item.description = 'hoge'
+          item.date = Time.now
+        }
+      }
+
+      make.image.title = 'makietan'
+      make.image.url = 'http://tanjo.in/img/makietan@144.png'
+
+      File.write('../rss.rdf', rss)
+    }
+  else
+    puts 'No Updates'
+  end
+
+end
